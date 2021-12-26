9c342fc141cc60e2f7d1040d33bf231cb6d93b95
Carlos Aguilera <ca@Carloss-MacBook-Air.local>
2021-12-25 18:28:49 -0800

    Init

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+class User
+{
+public:
+    std::string UserName;
+
+
+};
+
+void UserMenu()
+{
+    char UserMenuSelection {};
+
+   do
+   {
+       std::cout << "\n-------------------\n";
+       std::cout << "1. Write to file.\n";
+       std::cout << "2. Create a new file\n";
+       std::cout << "3. Delete file\n";
+       std::cout << "Q. Save and Quit.\n";
+
+       std::cout << "Enter Menu Selection: ";
+       std::cin >> UserMenuSelection;
+
+       switch (UserMenuSelection)
+       {
+           case '1':
+           {
+
+               break;
+           }
+           case '2':
+           {
+
+               break;
+           }
+           case '3':
+           {
+
+               break;
+           }
+           case 'Q':
+           {
+
+               break;
+           }
+           default:
+           {
+               std::cout << "Invalid Menu Selection! Try Again.\n";
+               break;
+           }
+       }
+   } while (UserMenuSelection != 'Q' && UserMenuSelection != 'q');
+    
+}
+
+int main()
+{
+    //std::string UserNameHolder;
+    //User Profile;
+    //Profile.UserName(UserNameHolder);
+
+}
