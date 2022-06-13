09b8714dc2d70f7831505386f20ed60bc1e23664
Carlos Aguilera <18aguilerac@gmail.com>
2022-06-12 21:12:01 -0700

    Example uri code

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 #include <string>
 
-#include <mongocxx/client.hpp>
-#include <mongocxx/instance.hpp>
-#include <mongocxx/uri.hpp>
-#include <mongocxx/options/client.hpp>
+// #include <mongocxx/client.hpp>
+// #include <mongocxx/instance.hpp>
+// #include <mongocxx/uri.hpp>
+// #include <mongocxx/options/client.hpp>
 
 int main() {
-  mongocxx::instance inst{};
-  const auto uri = mongocxx::uri{"mongodb+srv://averageJoe:hxYWatUPGcblcWU5@sandboxdatabase.ok6hp.mongodb.net/?retryWrites=true&w=majority"};
-  mongocxx::client conn{uri};
-  mongocxx::database db = conn["SandBox"];
+  // mongocxx::instance inst{};
+  // const auto uri = mongocxx::uri{"mongodb+srv://averageJoe:hxYWatUPGcblcWU5@sandboxdatabase.ok6hp.mongodb.net/?retryWrites=true&w=majority"};
+  // mongocxx::client conn{uri};
+  // mongocxx::database db = conn["SandBox"];
+  // if (db.has_collection(bsoncxx::v_noabi::string::view_or_value("Video Game")))
+  //   std::cout << "This works my guy\n";
 }
