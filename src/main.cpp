9046f9762b0d4c3c57fd4d2360c97870a2412e08
Carlos Aguilera <18aguilerac@gmail.com>
2022-06-14 22:28:47 -0700

    Need to code to much mathhhhhh

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 #include <string>
 
-// #include <mongocxx/client.hpp>
-// #include <mongocxx/instance.hpp>
-// #include <mongocxx/uri.hpp>
-// #include <mongocxx/options/client.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/options/client.hpp>
 
 int main() {
-  // mongocxx::instance inst{};
-  // const auto uri = mongocxx::uri{"mongodb+srv://averageJoe:hxYWatUPGcblcWU5@sandboxdatabase.ok6hp.mongodb.net/?retryWrites=true&w=majority"};
-  // mongocxx::client conn{uri};
-  // mongocxx::database db = conn["SandBox"];
-  // if (db.has_collection(bsoncxx::v_noabi::string::view_or_value("Video Game")))
-  //   std::cout << "This works my guy\n";
+  mongocxx::instance inst{};
+  const auto uri = mongocxx::uri{"mongodb+srv://averageJoe:hxYWatUPGcblcWU5@sandboxdatabase.ok6hp.mongodb.net/?retryWrites=true&w=majority"};
+  mongocxx::client conn{uri};
+  mongocxx::database db = conn["SandBox"];
+  db.create_collection(bsoncxx::v_noabi::string::view_or_value("Connor Goated at Unit Circle"));
+  if (db.has_collection(bsoncxx::v_noabi::string::view_or_value("Connor Goated at Unit Circle")))
+    std::cout << "This works my guy\n";
 }
