5e23e5bf44352fa63bfa3c5cfc52a417c5508ead
Carlos Aguilera <18aguilerac@gmail.com>
2022-06-19 15:37:32 -0700

    added ncurses dependency

---
+++ dependencies.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd lib
+git clone https://github.com/mongodb/mongo-c-driver.git
+cd mongo-c-driver
+git checkout 1.21.2
+mkdir cmake-build
+cd cmake-build
+cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
+if [[ "$OSTYPE" =~ ^msys ]]; then
+    sudowin cmake --build . --target install
+else
+    sudo cmake --build . --target install
+fi
+
+cd ../../
+rm -rf mongo-c-driver*
+tar -xf mongo-cxx-driver-r3.6.7.tar.gz
+cd mongo-cxx-driver-r3.6.7/build
+cmake ..                                \
+    -DCMAKE_BUILD_TYPE=Release          \
+    -DBSONCXX_POLY_USE_MNMLSTC=1        \
+    -DCMAKE_INSTALL_PREFIX=/usr/local
+if [[ "$OSTYPE" =~ ^msys ]]; then
+    sudowin cmake --build . --target install
+else
+    sudo cmake --build . --target install
+fi
+cd ../../
+rm -rf mongo-cxx-driver-r3.6.7*
+
+tar -xf ncurses-6.3.tar.gz
+cd ncurses-6.3/
+./configure
+make
+if [[ "$OSTYPE" =~ ^msys ]]; then
+    sudowin make install
+else
+    sudo make install
+fi
+
+cd ../
+rm -rf ncurses-6.3*
--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@
 #include <mongocxx/instance.hpp>
 #include <mongocxx/uri.hpp>
 #include <mongocxx/options/client.hpp>
+#include <ncurses.h>
 
 int main() {
   mongocxx::instance inst{};
   const auto uri = mongocxx::uri{"mongodb+srv://averageJoe:hxYWatUPGcblcWU5@sandboxdatabase.ok6hp.mongodb.net/?retryWrites=true&w=majority"};
   mongocxx::client conn{uri};
   mongocxx::database db = conn["SandBox"];
-  db.create_collection(bsoncxx::v_noabi::string::view_or_value("Connor Goated at Unit Circle"));
   if (db.has_collection(bsoncxx::v_noabi::string::view_or_value("Connor Goated at Unit Circle")))
     std::cout << "This works my guy\n";
+  initscr();
+  endwin();
 }
