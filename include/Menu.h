ba5eae37c5348ad4a6cb100db5c28550b92df2a3
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-04 10:45:53 -0700

    Menu.h documented

--- include/Menu.h
+++ include/Menu.h
@@ -10,26 +10,35 @@
 
 class Menu {
  public:
+  // TODO: Implementing the rule of 5
   Menu();
+  // Refer to documentation in Menu.cpp
   void clear();
   void CheckWindowSize();
   void PrintTitle(const std::string &Title) const;
 
+  // This is the bread and butter of printing the menu to the window
+  // a template it used to pass an array of any size (could have used template didnt think
+  // the extra overhead was worth it)
+  // using window attribute on and off to highlight the currently selected menu option
   template <std::size_t SIZE>
   void print(
       const Highlights &highlighted,
       const std::array<std::string, SIZE> &MenuSelection) const {
+    // Placement of text with respect of the y axis
     int8_t yPlacement = MenuSelection.size() / 2;
+    // loop starting at index 1 to avoid collisions with title
     for (int i = 1; i < MenuSelection.size(); ++i) {
+      // this is tenary since I might use a map later but checks if current index should
+      // be the one highlighted
       if (highlighted == Highlights(i)) wattron(m_Win, A_STANDOUT);
       mvwprintw(
           m_Win, (m_yMax / 4) - yPlacement, (m_xMax / 4) - (MenuSelection[i].size() / 2),
-          MenuSelection[i].c_str());  // Always 1 for y
+          MenuSelection[i].c_str());
       if (highlighted == Highlights(i)) wattroff(m_Win, A_STANDOUT);
       --yPlacement;
     }
   }
-
   WINDOW *m_Win;
 
  private:
