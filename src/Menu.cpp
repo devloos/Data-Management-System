4e694f54c82813dbfc34040a1a0126604294be27
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-04 10:37:55 -0700

    Menu.cpp documented

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,5 +1,6 @@
 #include "../include/menu.h"
 
+// Initializing window
 Menu::Menu() {
   initscr();
   noecho();
@@ -12,17 +13,25 @@ Menu::Menu() {
   m_Win = newwin(m_yMax / 2, m_xMax / 2, m_yMax / 4, m_xMax / 4);
   nodelay(m_Win, true);
 }
+// Clear window content
+// Required for spill of content when resizing
 void Menu::clear() { wclear(m_Win); }
+// Gets current size of window and compares it to "passed" y max and x max
+// if change then resize window
 void Menu::CheckWindowSize() {
   getmaxyx(stdscr, m_Height, m_Width);
+  // if either or
   if (m_yMax != m_Height || m_xMax != m_Width) {
     m_yMax = m_Height;
     m_xMax = m_Width;
+    // Based on ncurses documentation https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/
     wborder(m_Win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
     wrefresh(m_Win);
     endwin();
+    // new window created
     m_Win = newwin(m_yMax / 2, m_xMax / 2, m_yMax / 4, m_xMax / 4);
   }
+  // This method boxes window regardless since we call wclear every cycle
   box(m_Win, 0, 0);
 }
 
