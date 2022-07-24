4c6330e84b4997a79450e4b9587f54ead56a68c0
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-24 16:43:53 -0700

    added faculty class

---
+++ include/Faculty.h
@@ -0,0 +1,17 @@
+#ifndef FACULTY_H_
+#define FACULTY_H_
+#include "Person.h"
+
+enum AccessLevel { Teacher = 0, Proctor, Admin };
+
+class Faculty : public Person {
+ public:
+  Faculty(
+      const uint16_t &id, const std::string &password, const AccessLevel &accessLevel);
+
+ private:
+  AccessLevel accessLevel_;
+  uint8_t yearsWorking_;
+};
+
+#endif  // FACULTY_H_
