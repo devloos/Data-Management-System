3bc5a86f6f39e5a3d2fc279514db47b7bb9ec288
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-24 17:08:24 -0700

    implemented student class

---
+++ src/classes/Student.cpp
@@ -0,0 +1,26 @@
+#include "../../include/Student.h"
+
+Student::Student(
+    const uint16_t &id, const std::string &password, const uint8_t studentYear,
+    const float &GPA, const std::vector<std::string> &classSchedule,
+    const std::unordered_map<std::string, char> &grades,
+    const AcademicStatus &studentStatus)
+    : Person::Person(id, password),
+      studentYear_(studentYear),
+      GPA_(GPA),
+      classSchedule_(classSchedule),
+      grades_(grades),
+      studentStatus_(studentStatus) {}
+
+Student::Student(
+    const uint16_t &id, const std::string &password, const uint8_t studentYear,
+    const float &GPA, const std::vector<std::string> &classSchedule)
+    : Student(
+          id, password, studentYear, GPA, classSchedule,
+          std::unordered_map<std::string, char>(), Good_Standing) {}
+
+Student::Student(
+    const uint16_t &id, const std::string &password, const uint8_t studentYear)
+    : Student(
+          id, password, studentYear, 0.0f, std::vector<std::string>(),
+          std::unordered_map<std::string, char>(), Good_Standing) {}
